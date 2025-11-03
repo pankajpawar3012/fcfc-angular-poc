303fb45e72e61a79dbe2e691023d80d90ca7cb66
pankajpawar3012 <pankajpawar3012@gmail.com>
2025-11-02 21:34:07 -0500

    Adding vul function

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = 'fcfc-poc';
+
+  // VULNERABLE: Code Injection via eval()
+  runCode(userInput: string) {
+    eval(userInput); // Veracode will flag this as HIGH
+  }
+}
